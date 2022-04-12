0ceef5400f59a4371b943b0e14c068e12952168e
diegoyako <dyakobowicz@gmail.com>
2022-04-12 20:20:55 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"],
+  media: false, // or 'media' or 'class'
+  theme: {
+      extend: {},
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+};
